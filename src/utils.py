5eddf25a476c4bc1347ed337a9cd850b53de6974
makadiyadeep <deepmakadiya2004@gmail.com>
2025-08-12 19:10:33 +0530

    Front end done using Flask

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,12 @@
 import os
 import sys
 import dill
+import pickle
 
 import numpy as np
 import pandas as pd
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -25,9 +27,13 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            
-            model.fit(X_train,y_train)
+            para=param[list(models.key())[i]]
 
+            gs=GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+            
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
             #model.fit(X_train, y_train)  # Train model
 
             y_train_pred = model.predict(X_train)
@@ -41,6 +47,14 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
